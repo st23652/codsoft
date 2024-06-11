693dbb516e8a38e18d70d81972feb948fc36bc35
st23652 <149414885+st23652@users.noreply.github.com>
2024-06-11 23:02:24 +0400

    Task1.java

--- Task1.java
+++ Task1.java
@@ -13,41 +13,41 @@ public class Task1 {
             int attempts = 0;
             int totalAttempts = 5;
 
-            System.out.println("/nRound " + rounds + ":");
-            System.out.println("You have 5 attempts to guess the generated number.");
+            System.out.println("round " + rounds + ":");
+            System.out.println("you have 5 attempts to guess the generated number");
 
             while (attempts < totalAttempts) {
-                System.out.print("Attempt " + (attempts + 1) + ": Enter your guess: ");
+                System.out.print("attempt " + (attempts + 1) + ": enter your guess: ");
                 int userGuess = input.nextInt();
                 attempts++;
 
                 if (userGuess < numGenerated) {
-                    System.out.println("Too Low");
+                    System.out.println("too low");
                 } else if (userGuess > numGenerated) {
-                    System.out.println("Too High");
+                    System.out.println("too high");
                 } else {
-                    System.out.println("Guessed the right number.");
+                    System.out.println("guessed the right number");
                     userScore += (totalAttempts - attempts + 1);
                     break;
                 }
             }
 
             if (attempts == totalAttempts) {
-                System.out.println("You've used all " + totalAttempts + " attempts.");
-                System.out.println("The generated number was " + numGenerated + ".");
+                System.out.println("you have used all your" + totalAttempts + " attempts");
+                System.out.println("the generated number was " + numGenerated);
             }
 
-            System.out.print("Do you want to play another round? (yes/no): ");
-            String playAgain = input.next();
+            System.out.print("do you want to play another round (yes/no): ");
+            String newGame = input.next();
 
-            if (!playAgain.equalsIgnoreCase("yes")) {
+            if (!newGame.equalsIgnoreCase("yes")) {
                 break;
             }
 
             rounds++;
         }
 
-        System.out.println("/nGame over! Your final score is: " + userScore);
+        System.out.println("game over! your final score is: " + userScore);
         input.close();
     }
 }
